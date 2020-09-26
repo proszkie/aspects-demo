21a8215cfda185b8b110d6a44c1830c833bb642f
Piotr Proszowski <p.proszowski@gmail.com>
2020-09-26 17:40:18 +0200

    Initialize repo

---
+++ src/main/java/com/proszkie/aspectsdemo/AspectsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.proszkie.aspectsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proszkie/aspectsdemo/AspectsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proszkie.aspectsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
