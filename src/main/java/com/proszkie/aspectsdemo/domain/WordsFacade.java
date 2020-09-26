da4d86c78e3ab05fbecbc819e33bb5bdd7983c44
Piotr Proszowski <p.proszowski@gmail.com>
2020-09-26 19:48:07 +0200

    Add stopwatch

--- src/main/java/com/proszkie/aspectsdemo/domain/WordsFacade.java
+++ src/main/java/com/proszkie/aspectsdemo/domain/WordsFacade.java
@@ -1,22 +1,34 @@
 package com.proszkie.aspectsdemo.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
+@Slf4j
 public class WordsFacade {
 
     private final WordsTranslator wordsTranslator;
 
     public final Collection<WordWithTranslation> translate(final Collection<Word> words){
-        return words.stream()
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        final List<WordWithTranslation> wordsWithTranslations = words.stream()
                 .map(word -> WordWithTranslation.builder()
                         .word(word)
                         .translation(wordsTranslator.translate(word))
                         .build())
                 .filter(wwt -> wwt.getTranslation().isPresent())
                 .collect(Collectors.toList());
+
+        stopWatch.stop();
+        log.info("Words translated in {} ms. Pack size: {}", stopWatch.getTotalTimeMillis(), words.size());
+
+        return wordsWithTranslations;
     }
 }
